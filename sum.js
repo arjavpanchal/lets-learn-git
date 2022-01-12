5af75f3f30d686dd47e34b14716229628034a639
arjav <arjavp@zig.com>
2022-01-12 12:52:39 +0530

    adding sum.java

---
+++ sum.js
@@ -0,0 +1,10 @@
+
+const http = require('http');
+
+
+
+http.createServer(function (req,res)  {
+    res.writeHead(200, {'Content Type' : 'text/plain'});
+    res.end("hello world");
+
+}).listen(8080);
