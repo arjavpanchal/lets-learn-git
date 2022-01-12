ca318db0e34d88a350ac629aa704f638389a1c47
arjav <arjavp@zig.com>
2022-01-12 14:16:30 +0530

    added message in sum.js

--- diff.js
+++ diff.js
@@ -5,6 +5,6 @@ const http = require('http');
 
 http.createServer(function (req,res)  {
     res.writeHead(200, {'Content Type' : 'text/plain'});
-    res.end("hello world");
+    res.end("hello world!, welcome to my page");
 
 }).listen(8080);
